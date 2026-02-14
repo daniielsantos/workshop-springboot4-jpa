540bd24411f3939b4d7dfb1d5b4729042df1a404
Daniel <daniielsouzapvh@gmail.com>
2026-02-14 20:48:33 -0300

    Subtotal & Total methods

--- course/src/main/java/com/empose/course/entities/Payment.java
+++ course/src/main/java/com/empose/course/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.empose.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -16,7 +17,7 @@ public class Payment implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Instant moment;
-
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
